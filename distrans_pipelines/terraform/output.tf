fb7269b54cbfba97731e285394991334aa18dee4
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2023-04-07 15:44:39 -0400

    Added new terraform output variable to output agent vm admin username

--- distrans_pipelines/terraform/output.tf
+++ distrans_pipelines/terraform/output.tf
@@ -6,6 +6,10 @@ output "agent_public_ip_address" {
   value = azurerm_linux_virtual_machine.agent_vm.public_ip_address
 }
 
+output "agent_admin_username" {
+  value = azurerm_linux_virtual_machine.agent_vm.admin_username
+}
+
 output "tls_private_key" {
   value     = tls_private_key.agent_vm_ssh.private_key_pem
   sensitive = true
