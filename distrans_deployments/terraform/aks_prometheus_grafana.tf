6923126cb3b9e9d82767473fcdff035e7b56d25b
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2023-10-14 14:59:37 -0400

    Added sidecar to load custom grafana dashboard for distrans app

--- distrans_deployments/terraform/aks_prometheus_grafana.tf
+++ distrans_deployments/terraform/aks_prometheus_grafana.tf
@@ -16,3 +16,24 @@ resource "helm_release" "prometheus" {
   values           = [templatefile("./prometheus/install_values.yaml", { ws_public_ip = azurerm_windows_virtual_machine.app_vm.public_ip_address })]
   timeout          = 2000
 }
+
+resource "kubernetes_config_map" "grafana-dashboards-distrans" {
+  metadata {
+    name      = "grafana-dashboard-distrans"
+    namespace = kubernetes_namespace.prometheus_namespace.id
+
+    labels = {
+      grafana_dashboard = 1
+    }
+
+    annotations = {
+      k8s-sidecar-target-directory = "/tmp/dashboards/distrans"
+    }
+  }
+
+  data = {
+    "distrans-dashboard.json" = file("./prometheus/grafana_dashboard.json")
+  }
+
+  depends_on = [helm_release.prometheus]
+}
